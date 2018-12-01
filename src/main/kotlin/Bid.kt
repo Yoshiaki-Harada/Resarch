fa28a7c0e3431833c373206f31c3c99a2c52f282
haradayoshiaki <y.team.kobe@gmail.com>
2018-12-01 11:47:29 +0900

    データ構造の修正

--- src/main/kotlin/Bid.kt
+++ src/main/kotlin/Bid.kt
@@ -2,5 +2,5 @@
 //予算10.0で財0を数量2，財1を数量0, 財2を数量3希望する
 class Bid(
         val price: Double,
-        val bundle: ArrayList<ArrayList<Double>>
+        val bundle: ArrayList<Double>
 )
--- src/main/kotlin/BundleAdapter.kt
+++ src/main/kotlin/BundleAdapter.kt
@@ -4,18 +4,8 @@ import java.lang.reflect.Array
 
 class BundleAdapter {
     @ToJson
-    fun arrayListToJson(list: ArrayList<ArrayList<Double>>): List<List<Double>> = list
+    fun arrayListToJson(list: ArrayList<Double>): List<Double> = list
 
-    @FromJson
-    fun arrayListFromJson(list: List<List<Double>>): ArrayList<ArrayList<Double>> {
-        val arrayListDouble = ArrayList<Double>()
-        val arrayList = ArrayList<ArrayList<Double>>()
-        for (doubleList in list) {
-            for (double in doubleList){
-                arrayListDouble.add(double)
-            }
-            arrayList.add(arrayListDouble)
-        }
-        return arrayList
-    }
+    @FromJson fun arrayListFromJson(list: List<Double>) : ArrayList<Double> = ArrayList(list)
+
 }
