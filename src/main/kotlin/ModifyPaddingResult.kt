0f5e2d66aa4420e8b061b33a1fc83edb253ef95c
haradayoshiaki <y.team.kobe@gmail.com>
2019-11-02 20:00:42 +0900

    configの変更

--- src/main/kotlin/ModifyPaddingResult.kt
+++ src/main/kotlin/ModifyPaddingResult.kt
@@ -5,8 +5,8 @@ import writer.JsonWriter
 fun main() {
     for (i in 0..9) {
         println(i)
-        val rs = ResultConverter.fromJson(JsonImporter("Result/Provider=30/supply-100.0-200.0/$i/PaddingMethod/result").getString())
+        val rs = ResultConverter.fromJson(JsonImporter("Result/Provider=25/supply-100.0-200.0/$i/PaddingMethod/result").getString())
         val newRs = rs.copy(auctioneerProfit = rs.sumPay - rs.sumRevenue)
-        JsonWriter("Result/Provider=30/supply-100.0-200.0/$i/PaddingMethod/result").makeFile(ResultConverter.toJson(newRs))
+        JsonWriter("Result/Provider=25/supply-100.0-200.0/$i/PaddingMethod/result").makeFile(ResultConverter.toJson(newRs))
     }
 }
