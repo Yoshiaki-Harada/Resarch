835bde24e82de86d54afc8ed8bb3792dd0fc39d7
Yoshiaki Harada <yoshiaki.harada@uzabase.com>
2019-12-20 10:54:00 +0900

    参考文献をつけた

---
+++ Paper/master-thesis/src/template/others/auto/soturon.el
@@ -0,0 +1,22 @@
+(TeX-add-style-hook
+ "soturon"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "Suichoku"
+    "IoT2009"
+    "IoTとは（I23:online"
+    "Industrie4.0"
+    "IIC"
+    "IVI"
+    "Soumu-sharing"
+    "Hitachi-csmfg"
+    "KATSUMURA2016"
+    "FirstBuild"
+    "Factory2015"
+    "WU2013"
+    "WU2015"
+    "Ghomi2019"
+    "THEKINEN2017"
+    "CHIDA2019"))
+ :bibtex)
+
